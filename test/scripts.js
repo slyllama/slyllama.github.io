67ea7d7fb39fb91bee94afd134329482776c7be1
slyllama <alex.laing@me.com>
2024-08-20 16:54:49 +1200

    Improved title etc

--- test/scripts.js
+++ test/scripts.js
@@ -11,27 +11,9 @@ var mobileMenuBtnImg;
 
 function loadDOM() {
     // Load and configure DOM elements
-    headerLogo = document.getElementById("header-logo");
     mobileMenu = document.getElementById("mobile-menu");
-    mobileMenuBtn = document.getElementById("header-menu-btn");
     mobileMenuBtnImg = document.getElementById("header-menu-btn-img");
     mobileMenu.style.display = "none";
-
-    // Change the colour of the title when hovered
-    headerLogo.addEventListener("mouseover", (e) => {
-        headerLogo.src = "https://slyllama.net/test/assets/logo-hover.svg";
-    });
-    headerLogo.addEventListener("mouseout", (e) => {
-        headerLogo.src = "https://slyllama.net/test/assets/logo.svg";
-    });
-
-    // Change the colour of the mobile menu when hovered
-    mobileMenuBtn.addEventListener("mouseover", (e) => {
-        mobileMenuBtnImg.src = "https://slyllama.net/test/assets/mobile-menu-btn-hover.svg";  
-    });
-    mobileMenuBtn.addEventListener("mouseout", (e) => {
-        mobileMenuBtnImg.src = "https://slyllama.net/test/assets/mobile-menu-btn.svg";
-    });
 
     domLoaded = true;
 }
