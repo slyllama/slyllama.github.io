2a5768020693f43a697490d7719c854a744e671b
Alex Laing <alex.laing@me.com>
2024-08-26 22:57:12 +1200

    Fixed ordering in mobile mode

--- test/source/scripts.js
+++ test/source/scripts.js
@@ -12,10 +12,19 @@ var mobileMenuBtnImg;
 function loadDOM() {
     // Load and configure DOM elements
     mobileMenu = document.getElementById("mobile-menu");
+    mobileMenuBtn = document.getElementById("mobile-menu-btn");
     mobileMenuBtnImg = document.getElementById("header-menu-btn-img");
     mobileMenu.style.display = "none";
 
     domLoaded = true;
+
+    mobileMenuBtn.addEventListener("keydown", (e) => {
+        const { key } = e
+        if (key === 'Enter') {
+          e.preventDefault();
+          toggleMobileMenu();
+        }
+    });
 }
 
 addEventListener("resize", (e) => {
@@ -28,9 +37,17 @@ addEventListener("resize", (e) => {
 // Use the hamburger to show and hide the mobile menu
 function toggleMobileMenu() {
     if (domLoaded == false) { return; }
+    var menuElements = mobileMenu.getElementsByTagName("a");
+
     if (mobileMenu.style.display == "none") {
         mobileMenu.style.display = "block";
+        for (var m = 0; m < menuElements.length; m++) {
+            menuElements[m].removeAttribute('tabindex');
+        }
     } else {
         mobileMenu.style.display = "none";
+        for (var m = 0; m < menuElements.length; m++) {
+            menuElements[m].tabIndex = -1;
+        }
     }
 }
